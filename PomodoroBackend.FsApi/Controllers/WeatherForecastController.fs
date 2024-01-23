fc207a3fc1fa611ea96ae78b5910aed14479c4f4
hossein <h.ebr141@outlook.com>
2024-01-23 19:48:31 +0330

    F# setup

---
+++ PomodoroBackend.FsApi/Controllers/WeatherForecastController.fs
@@ -0,0 +1,47 @@
+﻿namespace PomodoroBackend.FsApi.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open PomodoroBackend.FsApi
+
+[<ApiController>]
+[<Route("[controller]")>]
+type WeatherForecastController (logger : ILogger<WeatherForecastController>) =
+    inherit ControllerBase()
+
+    let summaries =
+        [|
+            "Freezing"
+            "Bracing"
+            "Chilly"
+            "Cool"
+            "Mild"
+            "Warm"
+            "Balmy"
+            "Hot"
+            "Sweltering"
+            "Scorching"
+        |]
+
+    [<HttpGet>]
+    member _.Get() =
+        let rng = System.Random()
+        [|
+            for index in 0..4 ->
+                { Date = DateTime.Now.AddDays(float index)
+                  TemperatureC = rng.Next(-20,55)
+                  Summary = summaries.[rng.Next(summaries.Length)] }
+        |]
+        
+    [<HttpGet>]
+    [<Route("{daysAfterNow}")>]
+    member _.Get(daysAfterNow: int) =
+        let rng = Random()
+        
+        { Date = DateTime.Now.AddDays(daysAfterNow)
+          TemperatureC = rng.Next(-20, 55)
+          Summary = summaries.[rng.Next(summaries.Length)] }
